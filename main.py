b1a892794b9e12b80a762e2d3f4400fd0992dc1a
sra1kumar-NULL <62778911+sra1kumar-NULL@users.noreply.github.com>
2021-04-08 07:06:46 +0000

    created a file

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import Label,Button
+
+class App1:
+	def __init__(self,top):
+		self.top=top
+		top.title("Restaurant Management")
+		top.geometry("1028x500")
+
+root = tk.Tk()
+my_gui = App1(root)
+root.mainloop()
