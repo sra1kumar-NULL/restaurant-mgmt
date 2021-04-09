565e13ae8e0c3729c167a2b4e7156f1f93639d98
sra1kumar-NULL <sravansandeep163@gmail.com>
2021-04-09 16:57:54 +0530

    completed buttons

--- main.py
+++ main.py
@@ -26,25 +26,49 @@ class App1:
 		localtime1.place(relx=0.320,rely=0.10,height=51,width=507)
 		
 		self.Label12=tk.Label(master=top,text='Order Num',foreground="#bac8bd",font=font12,background="#091833")
-		self.Label12.place(relx=0.721,rely=0.25)
+		self.Label12.place(relx=0.321,rely=0.55)
 		
 		self.Label12=tk.Label(master=top,text='Fried Potato',foreground="#bac8bd",font=font12,background="#091833")
-		self.Label12.place(relx=0.621,rely=0.25)
+		self.Label12.place(relx=0.321,rely=0.50)
 		
 		self.Label12=tk.Label(master=top,text='Chk Burger',foreground="#bac8bd",font=font12,background="#091833")
-		self.Label12.place(relx=0.521,rely=0.25)
+		self.Label12.place(relx=0.321,rely=0.45)
 		
 		self.Label12=tk.Label(master=top,text='Big King',foreground="#bac8bd",font=font12,background="#091833")
-		self.Label12.place(relx=0.421,rely=0.25)
+		self.Label12.place(relx=0.321,rely=0.40)
 		
 		self.Label12=tk.Label(master=top,text='Chk Royal',foreground="#bac8bd",font=font12,background="#091833")
-		self.Label12.place(relx=0.321,rely=0.25)
+		self.Label12.place(relx=0.321,rely=0.35)
 		
 		self.Label12=tk.Label(master=top,text='Veg salad',foreground="#bac8bd",font=font12,background="#091833")
-		self.Label12.place(relx=0.221,rely=0.25)
+		self.Label12.place(relx=0.321,rely=0.30)
 
 		self.Label12=tk.Label(master=top,text='Drinks',foreground="#bac8bd",font=font12,background="#091833")
-		self.Label12.place(relx=0.111,rely=0.25)
+		self.Label12.place(relx=0.321,rely=0.25)
+		
+		self.entry1=tk.Entry(master=top,background="#d9d9d9",selectbackground="#f2a343",foreground="#c60000",font=font13)
+		self.entry1.place(relx=0.521,rely=0.55)
+		
+			
+		self.entry1=tk.Entry(master=top,background="#d9d9d9",selectbackground="#f2a343",foreground="#c60000",font=font13)
+		self.entry1.place(relx=0.521,rely=0.50)
+			
+		self.entry1=tk.Entry(master=top,background="#d9d9d9",selectbackground="#f2a343",foreground="#c60000",font=font13)
+		self.entry1.place(relx=0.521,rely=0.45)
+		
+			
+		self.entry1=tk.Entry(master=top,background="#d9d9d9",selectbackground="#f2a343",foreground="#c60000",font=font13)
+		self.entry1.place(relx=0.521,rely=0.40)
+			
+		self.entry1=tk.Entry(master=top,background="#d9d9d9",selectbackground="#f2a343",foreground="#c60000",font=font13)
+		self.entry1.place(relx=0.521,rely=0.35)
+			
+		self.entry1=tk.Entry(master=top,background="#d9d9d9",selectbackground="#f2a343",foreground="#c60000",font=font13)
+		self.entry1.place(relx=0.521,rely=0.30)
+			
+		self.entry1=tk.Entry(master=top,background="#d9d9d9",selectbackground="#f2a343",foreground="#c60000",font=font13)
+		self.entry1.place(relx=0.521,rely=0.25)
+		
 root = tk.Tk()
 my_gui = App1(root)
 root.mainloop()
