862933e0b02d67969080095a097bffdc551592db
sra1kumar-NULL <sravansandeep163@gmail.com>
2021-04-09 16:19:20 +0530

    added the buttons

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class App1:
 		
 		font10="{Courier New} 10 normal"
 		font11="{U.S. 101} 30 bold"
-		font12="A1 Aramco 11 bold"
+		font12="A1-Aramco 11 bold"
 		font13="{Courier New} 10 bold"
 		font14="{Segoe UI} 15 bold"
 		font15="Ariel 13 bold"
@@ -25,9 +25,26 @@ class App1:
 		
 		localtime1.place(relx=0.320,rely=0.10,height=51,width=507)
 		
+		self.Label12=tk.Label(master=top,text='Order Num',foreground="#bac8bd",font=font12,background="#091833")
+		self.Label12.place(relx=0.721,rely=0.25)
 		
+		self.Label12=tk.Label(master=top,text='Fried Potato',foreground="#bac8bd",font=font12,background="#091833")
+		self.Label12.place(relx=0.621,rely=0.25)
 		
+		self.Label12=tk.Label(master=top,text='Chk Burger',foreground="#bac8bd",font=font12,background="#091833")
+		self.Label12.place(relx=0.521,rely=0.25)
+		
+		self.Label12=tk.Label(master=top,text='Big King',foreground="#bac8bd",font=font12,background="#091833")
+		self.Label12.place(relx=0.421,rely=0.25)
+		
+		self.Label12=tk.Label(master=top,text='Chk Royal',foreground="#bac8bd",font=font12,background="#091833")
+		self.Label12.place(relx=0.321,rely=0.25)
+		
+		self.Label12=tk.Label(master=top,text='Veg salad',foreground="#bac8bd",font=font12,background="#091833")
+		self.Label12.place(relx=0.221,rely=0.25)
 
+		self.Label12=tk.Label(master=top,text='Drinks',foreground="#bac8bd",font=font12,background="#091833")
+		self.Label12.place(relx=0.111,rely=0.25)
 root = tk.Tk()
 my_gui = App1(root)
 root.mainloop()
